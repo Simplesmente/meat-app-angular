6c298e141740c5001789307b4a42f56a980af006
Andre Teles <andre.teles@paieterno.com.br>
2017-08-14 10:40:45 -0300

    finalizado module reactive forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
-import { FormGroup, FormBuilder} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators,AbstractControl} from '@angular/forms';
 
 import {RadioOption} from '../shared/radio/radio-option.model';
 
@@ -18,6 +18,10 @@ export class OrderComponent implements OnInit {
 
   orderForm: FormGroup;
 
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  numberPattern = /^[0-9]*$/;
+
   paymentsOptions:RadioOption[] = [
     {label:'Dinheriro',value:'MON'},
     {label:'Cartão de Débito',value:'DEB'},
@@ -33,13 +37,31 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
     
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      optionalAdress:this.formBuilder.control(''),
-      paymentsOptions: this.formBuilder.control('')
-    });
+      name: this.formBuilder.control('',[Validators.required,Validators.minLength(5)]),
+      email: this.formBuilder.control('',[Validators.required,Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('',[Validators.required,Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('',[Validators.required,Validators.minLength(5)]),
+      number: this.formBuilder.control('',[Validators.required,Validators.pattern(this.numberPattern)]),
+      optionalAddress:this.formBuilder.control(''),
+      paymentsOptions: this.formBuilder.control('',[Validators.required])
+    },{validator: OrderComponent.equalsTo});
+
+  }
+
+  static equalsTo(group:AbstractControl): {[key:string]:boolean}
+  {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+
+    if(!email || !emailConfirmation){
+        return undefined;
+    }
+
+    if(email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true};
+    }
+
+    return undefined;
 
   }
 
